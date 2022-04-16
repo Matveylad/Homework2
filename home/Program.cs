0ae10d33724cc95ba67fc06bf765c27dbfb1932d
Matvey <mat-lad@mail.ru>
2022-04-16 10:04:25 +0300

    add home2

--- home/Program.cs
+++ home/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+
+if (b>max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int a1 = 2;
+int b1 = 10;
+
+int max1 = a1;
+
+if (b1>max1) max1 = b1;
+
+Console.Write("max1 = ");
+Console.WriteLine(max1);
+
+int a2 = -9;
+int b2 = -3;
+
+int max2 = a2;
+
+if (b2>max2) max2 = b2;
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
---
+++ home2/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int a1 = 44;
+int b1 = 5;
+int c1 = 78;
+
+int max1 = a;
+
+if(a1>max1) max1=a1;
+if(b1>max1) max1=b1;
+if(c1>max1) max1=c1;
+Console.Write("max1 = ");
+Console.WriteLine(max1);
+
+int a2 = 22;
+int b2 = 3;
+int c2 = 9;
+
+int max2 = a2;
+
+if(a2>max2) max1=a2;
+if(b2>max2) max2=b2;
+if(c2>max1) max2=c2;
+Console.Write("max2 = ");
+Console.WriteLine(max2);
