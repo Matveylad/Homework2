89761c28e2dfb4f2aa0e3947e13d5a2aae5cc37c
Matvey <mat-lad@mail.ru>
2022-04-17 12:07:29 +0300

    classwork s3

---
+++ dz1-2/s3classwork/Program.cs
@@ -0,0 +1,41 @@
+﻿double X = new Random().Next(-100,100);
+double Y = new Random().Next(-100,100);
+Console.WriteLine(X);
+Console.WriteLine(Y);
+
+if (X<0 & Y<0)
+{
+    Console.WriteLine("Точка находится в 3й четверти");
+}
+else if(X>0 & Y>0)
+
+if (X>0 & Y>0)
+
+{
+    Console.WriteLine("Точка находится в 1й четверти");
+
+}
+else if(X<0 & Y<0)
+if (X<0 & Y>0)
+
+{
+    Console.WriteLine("Точка находится в 2й четверти");
+
+}
+
+if (X>0 & Y<0)
+else if(X<0 & Y>0)
+
+{
+    Console.WriteLine("Точка находится в 4й четверти");
+
+}
+
+if (X == 0| Y == 0)
+
+else if(X<0 & Y>0)
+{
+    Console.WriteLine("Точка не принадлежит конкртеной четверти");
+
+}
+
---
+++ dz1-2/s3classwork/s3classwork2/s3classwork3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int index=1;
+while (index<=N)
+{
+    if(index==N)
+    {
+        Console.Write($"{Math.Pow(index,2)}.");
+    }
+    else  
+        Console.Write($"{Math.Pow(index,2)},");
+    index++;
+}
+
+
+
+
