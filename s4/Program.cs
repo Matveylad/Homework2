e75fbb8e19492b9b53b6a97060a96e9759fd8fc8
Matvey <mat-lad@mail.ru>
2022-04-23 12:37:53 +0300

    s4 classwork

---
+++ s4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Веедите число N ");
+int N = int.Parse(Console.ReadLine()); 
+int pN = 1;
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i);
+    pN = pN *i;
+}
+Console.WriteLine(pN);
+  
---
+++ s4task3/Program.cs
@@ -0,0 +1,6 @@
+﻿int[]mass = new int[8];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random ().Next(0,2);
+    Console.Write(mass[i]);
+}
