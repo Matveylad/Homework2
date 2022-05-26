abd8aef641b2424a2e7522a22055886057f520d8
Matvey <mat-lad@mail.ru>
2022-05-26 23:29:46 +0300

    dz s 8

---
+++ home 8/Program.cs
@@ -0,0 +1,136 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// int m = 3, n = 3;
+
+// int[,] array = new int[m, n];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(1, 10);
+//     }
+// }
+
+// void Print(int[,] mass)
+// {
+//     for (int i = 0; i < mass.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < mass.GetLength(1); j++)
+//         {
+//             Console.Write(mass[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Print(array);
+// Console.WriteLine();
+// int temp;
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1) - 1; j++)
+//     {
+//         for (int k = j + 1; k < array.GetLength(1); k++)
+//         {
+//             if (array[i, j] < array[i, k])
+//             {
+//                 temp = array[i, k];
+//                 array[i, k] = array[i, j];
+//                 array[i, j] = temp;
+//             }
+//         }
+//     }
+// }
+
+
+// Print(array);
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
+// int m = 3, n = 4;
+// int m = 3, n = 4;
+
+// int[,] array = new int[m, n];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(1, 10);
+//     }
+// }
+// Console.WriteLine("Первоначальный массив");
+
+// void Print(int[,] mass)
+// {
+//     for (int i = 0; i < mass.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < mass.GetLength(1); j++)
+//         {
+//             Console.Write(mass[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Print(array);
+// Console.WriteLine();
+
+// int[] summ = new int[array.GetLength(1)];
+
+// for (int a = 0; a < array.GetLength(0); a++)
+// {
+//     for (int b = 0; b < array.GetLength(1); b++)
+//     {
+//         summ[a] = summ[a] + array[a, b];
+//     }
+//     Console.WriteLine($"Сумма {a + 1} cтроки равна: {summ[a]}");
+// }
+// int temp;
+// for (int i = 0; i < summ.Length; i++)
+// {
+//     for (int j = 0; j < summ.Length-1; j++)
+//     {
+//         if(summ[j]>summ[j+1])
+//         {
+//             temp=summ[j+1];
+//             summ[j+1]=summ[j];
+//             summ[j]=temp;
+//         }
+//     }
+// }
+// Console.Write($"Минимальная сумма строки равна: {summ[1]}");
+
+
+
+// Задача 60: Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+int m = 3, n = 3, k = 3;
+
+int[,,] array = new int[m, n, k];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int l = 0; l < array.GetLength(2); l++)
+        {
+            array[i, j, l] = new Random().Next(1, 10);
+        }
+
+    }
+}
+
+void Print(int[,,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            for (int l = 0; l < array.GetLength(2); l++)
+            {
+                Console.WriteLine($"{mass[i, j, l]} - m:{i}, n:{j}, k:{l}");
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
+Print(array);
---
+++ seminar 10/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+// первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5 -> 3 4 7 11 18
+// 6 и 10, N = 4 -> 6 10 16 26 
+// int a = 2, b = 9, N = 10;
+// int Fib(int f, int s, int num)
+// {
+//     if (num == 0)
+//         return f;
+//     if (num == 1)
+//         return s;
+//     return Fib(f, s, num - 1) + Fib(f, s, num - 2);
+// }
+// for (int i = 0; i < N; i++)
+// {
+//     int m = Fib(a, b, i);
+//     Console.Write(m + " ");
+// }
+
+// Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с»
+// и «в». Покажите все слова, состоящие из n букв, которые можно построить из
+// букв этого алфавита.
+// n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+// ав, ва, ви, ив, св, вс
+
+
+
+// int n =1;
+
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//  if (length == word.Length)
+//  {
+//  Console.WriteLine($"{n++} {new String(word)}"); return;
+//  }
+//  for (int i = 0; i < alphabet.Length; i++)
+//  {
+//  word[length] = alphabet[i];
+//  FindWords(alphabet, word, length + 1);
+//  }
+// }
+// FindWords("аисв", new char[2]);
+
+
+// Задача 72: Заданы 2 массива: info и data. В массиве info хранятся двоичные
+// представления нескольких чисел (без разделителя). В массиве data хранится
+// информация о количестве бит, которые занимают числа из массива info.
+// Напишите программу, которая составит массив десятичных представлений чисел
+// массива data с учётом информации из массива info.
+// 25 мин
+// входные данные:
+// - data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+// - info = {2, 3, 3, 1 }
+// выходные данные:
+// - 1, 7, 0, 1
+
+int[] array1 = { 0, 1, 1, 1, 1, 0, 0, 0, 1 }; // data
+int[] array2 = { 2, 3, 3, 1 }; // info
+
+int Transfer(int[] data, int[] info)
+{
+    int count = 0;
+    int[] result = new int[info.Length];
+    for (int i = 0; i < info.Length; i++)
+    {
+
+        for (int j = count; j < count + info[i]; j++)
+        {
+            result[i] += data[j] * (int)Math.Pow(2, info[i] + count - j - 1);
+
+
+        }
+        count += info[i];
+        Console.Write(result[i] + " ");
+
+    }
+    return result[0];
+
+}
+Transfer(array1, array2);
